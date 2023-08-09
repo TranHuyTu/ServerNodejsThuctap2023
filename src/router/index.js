fe393aa67bb012cc5f06d117488dde74b57c8508
Trần Huy Tú <94135480+TranHuyTu@users.noreply.github.com>
2023-08-09 16:42:15 +0700

    demo-3

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,9 @@ const newsRouter = require("./news");
 const siteRouter = require("./site");
 
 function router(app) {
-    // app.get("/", (req, res) => {
-    //     res.render("home");
-    // });
+    app.get("/", (req, res) => {
+        res.render("home");
+    });
     app.use("/new", newsRouter);
     app.use("/", siteRouter);
 }
