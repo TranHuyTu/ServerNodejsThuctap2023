2ec50814d330f0406ce55ef5def2a14212350925
Trần Huy Tú <94135480+TranHuyTu@users.noreply.github.com>
2023-11-29 21:33:46 +0700

    kiemthuloginfull

--- src/app/controller/login.controller.js
+++ src/app/controller/login.controller.js
@@ -81,16 +81,47 @@ class login {
                     async (err, hash) => {
                         if (err) {
                             console.error("Error hashing password");
-                            return;
+                            res.status(401).send({
+                                register_status: "Error comparing passwords",
+                            });
+                        } else {
+                            await User.find({
+                                email: req.body.email,
+                            }).then(async (user) => {
+                                if (!user[0]) {
+                                    const newUser = new User({
+                                        salt: salt,
+                                        ...req.body,
+                                        password: hash,
+                                    });
+                                    const savedUser = await newUser.save();
+                                    console.log(savedUser);
+                                    res.status(200).send(newUser);
+                                } else {
+                                    bcrypt.compare(
+                                        req.body.password + user[0].salt,
+                                        user[0].password,
+                                        async (err, result) => {
+                                            if (err) {
+                                                console.error(
+                                                    "Error comparing passwords:",
+                                                    err,
+                                                );
+                                                res.status(401).send({
+                                                    register_status:
+                                                        "Error comparing passwords",
+                                                });
+                                            } else {
+                                                res.status(401).send({
+                                                    register_status:
+                                                        "Register False",
+                                                });
+                                            }
+                                        },
+                                    );
+                                }
+                            });
                         }
-                        const newUser = new User({
-                            salt: salt,
-                            ...req.body,
-                            password: hash,
-                        });
-                        const savedUser = await newUser.save();
-                        console.log(savedUser);
-                        res.send(newUser);
                     },
                 );
             } catch (error) {
@@ -109,40 +140,52 @@ class login {
                     // Số vòng lặp để tăng độ phức tạp của quá trình hash (càng cao càng tốn thời gian)
                     const saltRounds = 10;
 
-                    bcrypt.hash(
-                        req.body.password + user.salt,
-                        saltRounds,
-                        async (err, hash) => {
-                            if (err) {
-                                console.error("Error hashing password");
-                                return;
-                            }
-                            const newUser = new User({
-                                _id: mongoose.Types.ObjectId.createFromHexString(
-                                    req.params.id,
-                                ),
-                                name: req.body.name,
-                                email: req.body.email,
-                                password: hash,
-                                salt: user.salt,
-                            });
-                            console.log(req.params.id, newUser);
+                    if (req.body.password || req.body.email || req.body.name) {
+                        bcrypt.hash(
+                            req.body.password + user.salt,
+                            saltRounds,
+                            async (err, hash) => {
+                                if (err) {
+                                    console.error("Error hashing password");
+                                    res.status(401).send({
+                                        update_status:
+                                            "Error comparing passwords",
+                                    });
+                                } else {
+                                    const newUser = new User({
+                                        _id: mongoose.Types.ObjectId.createFromHexString(
+                                            req.params.id,
+                                        ),
+                                        name: req.body.name,
+                                        email: req.body.email,
+                                        password: hash,
+                                        salt: user.salt,
+                                    });
+                                    console.log(req.params.id, newUser);
 
-                            const updatedUser = await User.updateOne(
-                                {
-                                    _id: mongoose.Types.ObjectId.createFromHexString(
-                                        req.params.id,
-                                    ),
-                                },
-                                newUser,
-                                { new: true },
-                            );
-                            res.status(200).send(updatedUser);
-                        },
-                    );
+                                    const updatedUser = await User.updateOne(
+                                        {
+                                            _id: mongoose.Types.ObjectId.createFromHexString(
+                                                req.params.id,
+                                            ),
+                                        },
+                                        newUser,
+                                        { new: true },
+                                    );
+                                    res.status(200).send(updatedUser);
+                                }
+                            },
+                        );
+                    } else {
+                        res.status(401).send({
+                            update_status: "Error data form input",
+                        });
+                    }
                 });
             } catch (error) {
-                res.status(500).json({ error: error.message }); // Xử lý lỗi nếu có
+                res.status(401).send({
+                    update_status: "Error user does not exist",
+                });
             }
         }
 
